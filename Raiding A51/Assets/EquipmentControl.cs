faa6225023ae1a2336042890702806812feaa9fc
Birshan Ramzy Caraccio <47294281+birshan@users.noreply.github.com>
2019-11-12 23:14:46 +0530

    Level0.v0.1
    Level0 scene with equipment wheel included to swap between drone, rc car and player character.

---
+++ Raiding A51/Assets/EquipmentControl.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquipmentControl : MonoBehaviour
+{
+    public GameObject[] equipment;
+    // Start is called before the first frame update
+    public int current = 0;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Alpha1))
+        {
+            current = 0;
+        }
+        if (Input.GetKey(KeyCode.Alpha2))
+        {
+            current = 1;
+        }
+        if (Input.GetKey(KeyCode.Alpha3))
+        {
+            current = 2;
+        }
+
+
+        for(int i = 0; i<equipment.Length; i++)
+        {
+            if(i == current)
+            {
+                equipment[i].SetActive(true);
+            }
+            else
+            {
+                equipment[i].SetActive(false);
+            }
+        
+        }
+    }
+}
